8ddeb343730d8e987dc6f0c6a2352e878019587a
Ravichandra <matrixravi@gmail.com>
2017-07-28 13:20:48 +0200

    Download file functionality v2 : File will be renamed to include upload timestamp

--- demo/demo/models.py
+++ demo/demo/models.py
@@ -2,7 +2,14 @@ from django.db import models
 #from django.contrib.auth.models import User
 from django.conf import settings
 
+import datetime
+
+def content_file_name(instance, filename):
+    name, ext = filename.split('.')
+    filename = "%s_%s.%s" % (name, datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S"), ext)
+    return filename
+
 class DropZoneModel(models.Model):
     #user = models.ForeignKey(User, null=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
-    file = models.FileField(upload_to='')
+    file = models.FileField(upload_to=content_file_name)
